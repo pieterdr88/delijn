186a29182bc8fe33109109b060e3870bfb867a96
pieterdr88 <74308639+pieterdr88@users.noreply.github.com>
2024-08-24 15:45:50 +0200

    Update SceneDelegate.swift

--- Koala-Storyboard/SceneDelegate.swift
+++ Koala-Storyboard/SceneDelegate.swift
@@ -23,8 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(
-                rootView: LandmarkList()
-                    .environmentObject(UserData())
+                rootView: Instellingen()
             )
             self.window = window
             window.makeKeyAndVisible()
