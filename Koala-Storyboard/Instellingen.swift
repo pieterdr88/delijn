620794934fdc8ec1ab4af519b40e258b9d03ffc0
pieterdr88 <74308639+pieterdr88@users.noreply.github.com>
2024-08-24 16:12:36 +0200

    Update Instellingen.swift

--- Koala-Storyboard/Instellingen.swift
+++ Koala-Storyboard/Instellingen.swift
@@ -3,6 +3,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Delijn 0.001")
+        VStack {
+            Text("Delijn 0.001")
+        }
+        .padding()
     }
 }
