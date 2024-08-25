26fcf958b76f87d895900b692ed256f817004e9a
pieterdr88 <74308639+pieterdr88@users.noreply.github.com>
2024-08-25 18:19:16 +0200

    Update ContentView.swift

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Form {
+            Text("Welkom")
+        }
         VStack {
             Image("gatlinburgtn")
                 .resizable()
