3b77c050a58df178e7773ab1c1ef127b8dae16aa
pieterdr88 <74308639+pieterdr88@users.noreply.github.com>
2024-08-26 11:08:46 +0200

    Update ContentView.swift

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -2,39 +2,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-
-        NavigationLink(destination: SecondView()) {
+        
+        NavigationView{
+            NavigationLink(destination: SecondView()) {
             Text("Go to Second View")
         }
         
 
         VStack {
-        Image("gatlinburgtn")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .cornerRadius(50)
-            .padding(.all)
+            Image("gatlinburgtn")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(50)
+                .padding(.all)
             
-        Text("delijn 0.001")
-            .font(.largeTitle)
-            .foregroundColor(.blue)
+            Text("delijn 0.001")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
             
-        Text("In de toekomst zal je kunnen kijken wanneer je bus er is")
-            .font(.title)
-            .foregroundColor(.blue)
-        .padding()
-        Form{
-            ForEach(1..<50) { i in
-            Text("test \(i)")
-            } 
+            Text("In de toekomst zal je kunnen kijken wanneer je bus er is")
+                .font(.title)
+                .foregroundColor(.blue)
+            .padding()
+            Form{
+                ForEach(1..<50) { i in
+                    Text("test \(i)")
+                    } 
+                }
+            
+            Text("dit is een tekst")
+                .background(Color.green)
+
+            }
+            .navigationTitle("welkom")
         }
             
-        Text("dit is een tekst")
-            .background(Color.green)
-
-        }            
-            
-        .navigationTitle("First Navigation Title")
     }
 }
 
