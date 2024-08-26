8af2b5765b2501d9d65f524982e00c415444b70c
pieterdr88 <74308639+pieterdr88@users.noreply.github.com>
2024-08-26 10:26:25 +0200

    Update ContentView.swift

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -3,39 +3,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 
-        NavigationView {
-            NavigationLink(destination: SecondView()) {
-                Text("Go to Second View")
-            }
+        NavigationLink(destination: SecondView()) {
+            Text("Go to Second View")
+        }
         
 
-            VStack {
-            Image("gatlinburgtn")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(50)
-                .padding(.all)
+        VStack {
+        Image("gatlinburgtn")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .cornerRadius(50)
+            .padding(.all)
             
-            Text("delijn 0.001")
-                .font(.largeTitle)
-                .foregroundColor(.blue)
+        Text("delijn 0.001")
+            .font(.largeTitle)
+            .foregroundColor(.blue)
             
-            Text("In de toekomst zal je kunnen kijken wanneer je bus er is")
-                .font(.title)
-                .foregroundColor(.blue)
-            .padding()
-            Form{
-               ForEach(1..<50) { i in
-                Text("test \(i)")
-                } 
-            }
+        Text("In de toekomst zal je kunnen kijken wanneer je bus er is")
+            .font(.title)
+            .foregroundColor(.blue)
+        .padding()
+        Form{
+            ForEach(1..<50) { i in
+            Text("test \(i)")
+            } 
+        }
             
-            Text("dit is een tekst")
-                .background(Color.green)
+        Text("dit is een tekst")
+            .background(Color.green)
 
-            }            
+        }            
             
-        }.navigationBarTitle("First View")
+        .navigationTitle("First Navigation Title")
     }
 }
 
