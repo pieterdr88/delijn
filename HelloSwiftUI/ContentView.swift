cbd6930b851d0dbb62ce78e1a920d965b257daad
pieterdr88 <74308639+pieterdr88@users.noreply.github.com>
2024-08-28 10:28:55 +0200

    Update ContentView.swift

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -1,39 +1,41 @@
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
         NavigationView{
-            VStack {
-            Image("gatlinburgtn")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(50)
-                .padding(.all)
-            
-            Text("delijn 0.001")
-                .font(.largeTitle)
-                .foregroundColor(.blue)
-            
-            Text("In de toekomst zal je kunnen kijken wanneer je bus er is")
-                .font(.title)
-                .foregroundColor(.blue)
-            .padding()
-            Form{
-                ForEach(1..<50) { i in
-                    Text("test \(i)")
-                    } 
+            ScrollView{
+                VStack {
+                Image("gatlinburgtn")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(50)
+                    .padding(.all)
+                
+                Text("delijn 0.001")
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+                
+                Text("In de toekomst zal je kunnen kijken wanneer je bus er is")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                .padding()
+                Form{
+                    ForEach(1..<50) { i in
+                        Text("test \(i)")
+                        } 
+                    }
+                
+                Text("dit is een tekst")
+                    .background(Color.green)
+
+                NavigationLink(destination:SecondView(),label:{
+                    Text("Open scherm 2")
+                })
+
                 }
-            
-            Text("dit is een tekst")
-                .background(Color.green)
-
-            NavigationLink(destination:SecondView(),label:{
-                Text("Open scherm 2")
-            })
-
+                .navigationTitle("welkom")
             }
-            .navigationTitle("welkom")
         }
             
     }
